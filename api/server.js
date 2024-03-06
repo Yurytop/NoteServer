664cfe3a1b43e697497efdd6fd4e545be69d5f57
Yurytop <yakshin.mo@yandex.ru>
2024-03-06 17:12:07 +0300

    add server

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('notes.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/notes/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+module.exports = server
