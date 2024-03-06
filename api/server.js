df2c6497abca70cd90e6e03b5815017283be58f0
Yurytop <yakshin.mo@yandex.ru>
2024-03-06 23:32:21 +0300

    cors 14

--- api/server.js
+++ api/server.js
@@ -1,8 +1,15 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('notes.json')
-const middlewares = jsonServer.defaults({noCors: true})
+const middlewares = jsonServer.defaults()
 
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 server.use(middlewares)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
