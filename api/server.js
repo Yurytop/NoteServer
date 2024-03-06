6944e8e683a41067eee4f4d81861de0fab8e6830
Yurytop <yakshin.mo@yandex.ru>
2024-03-06 23:08:23 +0300

    cors 11

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('notes.json')
-const middlewares = jsonServer.defaults({noCors: true})
+// const middlewares = jsonServer.defaults({noCors: true})
 
-server.use(middlewares)
+server.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/notes/:resource/:id/show': '/:resource/:id'
