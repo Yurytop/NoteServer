29f56dea803d32898ee860cc1b0ce79c232eb5cb
Yurytop <yakshin.mo@yandex.ru>
2024-03-07 17:41:27 +0300

    cors 17

--- api/server.js
+++ api/server.js
@@ -2,8 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('notes.json')
 const middlewares = jsonServer.defaults()
-
-
+const cors = require('cors');
+    
+        
+server.use(cors());
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', '*');
