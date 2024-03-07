89724597d9c577785a49a3f9b196e5874416f1ff
Yurytop <yakshin.mo@yandex.ru>
2024-03-07 17:35:08 +0300

    cors 15

--- api/server.js
+++ api/server.js
@@ -5,9 +5,9 @@ const middlewares = jsonServer.defaults()
 
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', 'https://notes-mauve-mu.vercel.app/');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
   });
 server.use(middlewares)
