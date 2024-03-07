3edc2c68b353aff4d10d2875689ff6c0a32b2faf
Yurytop <yakshin.mo@yandex.ru>
2024-03-07 18:17:54 +0300

    cors 19

--- api/server.js
+++ api/server.js
@@ -2,18 +2,14 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('notes.json')
 const middlewares = jsonServer.defaults()
-const cors = require('cors');
-    
 
-server.use(
-    cors({
-        origin: true,
-        credentials: true,
-        preflightContinue: false,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    })
-);
-server.options('*', cors());
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://notes-mauve-mu.vercel.app');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 server.use(middlewares)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
