d2544b5a1d11cb52646da9bdd048b6100cf9944e
Yurytop <yakshin.mo@yandex.ru>
2024-03-09 23:09:45 +0300

    cors 24

--- api/server.js
+++ api/server.js
@@ -21,12 +21,12 @@ server.use(cors({
 
 server.options('*', cors());
 
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://notes-mauve-mu.vercel.app');
-//   res.header('Access-Control-Allow-Methods', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 server.listen(3000, () => {
     console.log('JSON Server is running')
