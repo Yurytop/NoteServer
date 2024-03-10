c5840defc53c86a92b74f864802bf6fca6e03911
Yurytop <yakshin.mo@yandex.ru>
2024-03-10 21:14:19 +0300

    tmp add2

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('api/tmp/notes.json')
+const router = jsonServer.router('tmp/notes.json')
 const middlewares = jsonServer.defaults()
 const cors = require('cors');
 
